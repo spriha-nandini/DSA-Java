c01f50539f46327483e4c262759eae59f76dc1d9
spriha-nandini <sprihanandini8114@gmail.com>
2026-09-12 02:50:46 +0530

    Initial DSA structure

---
+++ LeetCode/1_TwoSum.java
@@ -0,0 +1,17 @@
+package LeetCode;
+
+public class 1_TwoSum {
+    class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int n=nums.length;
+        for (int i=0;i<n;i++){
+            for (int j=i+1;j<n;j++){
+                if(nums[i]+nums[j]==target){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+}
+}
